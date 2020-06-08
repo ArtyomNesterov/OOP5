18de2395d51cdf835006a9f83394cbd07c5acf59
Артём Нестеров <ehot03@bk.ru>
2020-06-08 15:59:24 +0500

    Добавьте файлы проекта.

---
+++ ООП 5/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ООП_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Tobi", "Mou");
+            Employee emp = new Employee("Paul", "Robbers", "Yandex");
+
+            p.Display();
+
+            emp.Display();
+            Console.ReadKey();
+        }
+    }
+    class Employee : Person
+    {
+        private string company;
+
+        public Employee(string name, string surname, string comp)
+            : base(name, surname)
+        {
+            company = comp;
+        }
+
+        public string Company
+        {
+            get { return company; }
+            set { company = value; }
+        }
+        public void DisplayB()
+        {
+            Console.WriteLine(Name + " | " + Surname + " | " + Company);
+        }
+    }
+    class Person
+    {
+        private string name;
+        private string surname;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+        public void Display()
+        {
+            Console.WriteLine(Name + " " + Surname);
+        }
+        public Person(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+    }
+}
